734033dc8b09fb0662f82bb4daff406acb40e4b2
nsljivic1 <nsljivic1@etf.unsa.ba>
2015-05-12 11:19:50 +0200

    Kod za WPF (ne sve klase do sada)

---
+++ Teatar64 - VisualStudio WPF/Teatar64/Osoba.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teatar64
+{
+    class Osoba
+    {
+        private string ime { get; set; }
+        private string prezime { get; set; }
+        public string DajIme() { return ime; }
+        public string DajPrezime() { return prezime; }
+    }
+}
