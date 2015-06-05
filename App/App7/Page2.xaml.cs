24f3bc4f4ae79a2f80dd0671816d46a32f4e3c1d
Delila Smajlović <dsmajlovic1@etf.unsa.ba>
2015-06-05 23:49:10 +0200

    Mobilna aplikacija

---
+++ App/App7/Page2.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace App7
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Page2 : Page
+    {
+        public Page2()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+        }
+
+        private void Koment1(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Page3));
+        }
+
+        private void Koment2(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Page3));
+        }
+
+        private void Koment3(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(Page3));
+        }
+    }
+}
